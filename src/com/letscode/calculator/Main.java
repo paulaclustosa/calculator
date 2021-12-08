c04ba048633666e144c6160c160759bcc914b4bb
Paula Lustosa <pl.lustosa@gmail.com>
2021-12-07 23:01:48 -0300

    Simple display change on Main

--- src/com/letscode/calculator/Main.java
+++ src/com/letscode/calculator/Main.java
@@ -8,4 +8,5 @@ public class Main {
     public static void main(String[] args) {
         Operations.calculateOperation(Reader.getOperator(), Reader.getFirstOperand(), Reader.getSecondOperand());
     }
+
 }
