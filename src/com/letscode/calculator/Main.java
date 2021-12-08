aef944e59a6b9d4c1cc68134b604a5d8f5ba7435
Paula Lustosa <pl.lustosa@gmail.com>
2021-12-07 23:01:33 -0300

    Simple display change on Main

--- src/com/letscode/calculator/Main.java
+++ src/com/letscode/calculator/Main.java
@@ -1,16 +1,11 @@
 package com.letscode.calculator;
 
 import com.letscode.calculator.service.Operations;
-import com.letscode.calculator.utils.Printer;
 import com.letscode.calculator.utils.Reader;
-
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-
         Operations.calculateOperation(Reader.getOperator(), Reader.getFirstOperand(), Reader.getSecondOperand());
-
     }
 }
